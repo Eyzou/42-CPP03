e57dfa184d5a0de926b435714b8b18cc86adc43d
Eyzou <elodie.hamm@gmail.com>
2024-07-09 09:11:34 +0200

    hi

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,6 @@ int main ()
     ClapTrap clap1("elo la goat");
 
     std::cout << "****TESTING**** " << std::endl;
-    std::cout << "First ClapTrap name is playing " << std::endl;
     clap1.attack("peppa pig");
     clap1.takeDamage(8);
     clap1.takeDamage(3);
@@ -14,7 +13,6 @@ int main ()
     clap1.attack("peppa pig 2");
 
     std::cout << std::endl;
-    std::cout <<"Then Default ClapTrap is playing" << std::endl;
     ClapTrap clap2;
     clap2.beRepaired(1);
     clap2.attack("peppa pig sister");
